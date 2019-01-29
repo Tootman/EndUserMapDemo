9823ab1f78c10340d47b64cff4e072ecf15018b0
Dan Simmons <danieljsimmons@btinternet.com>
2019-01-29 15:30:53 +0000

    geo type mouse cursor

--- js/main.js
+++ js/main.js
@@ -17,149 +17,73 @@
     trackUserLocation: true
   }));
 
-  map.on('load', () => {
+  let lineLayers = [
+    'lines- walls',
+    'lines - fences',
+    'fence over wall',
+    'lines - paths',
+    'lines - all other'
+  ] // replace this with the name of the layer
 
-    /*
-          map.addLayer({
-            "id": "myPoints",
-            "type": "circle",
-            "minzoom": 17,
-            "maxzoom": 19,
-            "source": {
-              type: 'vector',
-              url: 'mapbox://dansimmons.6klk60z4'
-            },
-            "source-layer": "RichmondPointsAll-bjmuam",
-            "paint": {
-              "circle-radius": 3,
-              "circle-stroke-width": 1,
-              "circle-stroke-color": "white"
-            }
-          });
-    */
+  let allLayers = lineLayers
+  allLayers.push('richmondpointsall')
+  allLayers.push('richmondpolygonsall-74pg99')
+  //allLayers.push('archive-polygons-cb5y86')
+  lineLayers.map(layer => {
+    map.on('mouseenter', layer, e => {
+      map.getCanvas().style.cursor = 'cell';
+    })
+    map.on('mouseleave', layer, () => {
+      map.getCanvas().style.cursor = '';
+    });
+  })
 
-    /*
-          map.addLayer({
-            "id": "mySymbols",
-            "type": "symbol",
-            "minzoom": 19,
-            "maxzoom": 24,
-            "source": {
-              type: 'vector',
-              url: 'mapbox://dansimmons.6klk60z4'
-            },
-            "source-layer": "RichmondPointsAll-bjmuam",
-            "layout": {
-              //"icon-image":["case",["get", "ASSET"], "Bench", "park%20bench2"],
+  map.on('mouseenter', "richmondpointsall", e => {
+    map.getCanvas().style.cursor = 'cell';
+  })
+  map.on('mouseleave', "richmondpointsall", () => {
+    map.getCanvas().style.cursor = '';
+  });
 
 
-              "icon-image": [
-                "match",
-                ["get", "ASSET"], "Bench", "park%20bench2",
-                "Litter bin", "litter%20bin",
-                "Lighting column", "lighting%20column",
-                "Inspection cover", "inspection%20cover",
-                "gulley%20grating"
-              ],
 
-              "icon-size": 1,
-              "text-field": ["match",["get", "ASSET"], ["Bench", "Lighting column","Inspection cover", "Gulley grating", "Litter bin"],"",["get", "ASSET"]],
-              "symbol-placement": "point",
-              "text-anchor": "left",
-              "text-offset": [1, 0],
-              // "text-allow-overlap": true,
-              "icon-allow-overlap": true,
-              "text-optional": true,
-              "text-size": 10
-            }
+  map.on('click', e => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: allLayers
+    });
 
-          });
+    if (!features.length) {
+      return;
+    }
 
-    */
-    /*
-          map.addLayer({
-            "id": "myLines",
-            "type": "symbol",
-            "minzoom": 14,
-            "maxzoom": 24,
-            "source": {
-              type: 'vector',
-              url: 'mapbox://dansimmons.6klk60z4'
-            },
-            "source-layer": "Richmondlinesall",
-            "layout": {
-              //"icon-image":["case",["get", "ASSET"], "Bench", "park%20bench2"],
-              "icon-size": 1,
-              "text-field": ["match",["get", "ASSET"], ["Bench", "Lighting column","Inspection cover", "Gulley grating", "Litter bin"],"",["get", "ASSET"]],
-              "symbol-placement": "point",
-              "text-anchor": "left",
-              "text-offset": [1, 0],
-              // "text-allow-overlap": true,
-              "icon-allow-overlap": true,
-              "text-optional": true,
-              "text-size": 10
-            }
+    const feature = features[0];
+    const propSet = f => {
+      return Object.keys(f.properties)
+        .map(item => {
+          return `${item}: ${f.properties[item]}`;
+        })
+        .join("<br>");
+    };
+    const p = feature.properties
+    const popupTitle = p.ASSET || p.Asset || p.asset
+    const popupContent = `<h3>${popupTitle}</h3>${propSet(feature)}`
+    const popup = new mapboxgl.Popup({
+        offset: [0, -15]
+      })
+      .setLngLat(e.lngLat)
+      .setHTML(popupContent)
+      .addTo(map);
+  });
 
-          });
-
-      */
+  // https://www.mapbox.com/mapbox-gl-js/example/filter-features-within-map-view/
+  map.on('moveend', () => {
+    const features = map.queryRenderedFeatures({
+      layers: ['richmondpointsall']
+    })
+    //console.log(features)
 
   });
 
-  /*
-  map.addControl(new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-       bbox: [-0.34418089928223594,51.44143958893025,-0.210392128369989,51.50711685536709], // Boundary for
-      proximity: {
-        longitude: -0.2840893326948617,
-        latitude:51.46828688632934
-    }
-  }));
-  */
-
-  map.on('click', e => {
-        const features = map.queryRenderedFeatures(e.point, {
-            layers: ['richmondpointsall',
-              'richmondpolygonsall-2v4029',
-            'lines- walls',
-            'lines - fences',
-            'fence over wall',
-            'lines - all other'
-            ] // replace this with the name of the layer
-            });
-
-          if (!features.length) {
-            return;
-          }
-
-          const feature = features[0];
-          const propSet = f => {
-            return Object.keys(f.properties)
-              .map(item => {
-                return `${item}: ${f.properties[item]}`;
-              })
-              .join("<br>");
-          };
-          const p =  feature.properties
-          const popupTitle = p.ASSET || p.Asset || p.asset
-          const popupContent = `<h3>${popupTitle}</h3>${propSet(feature)}`
-          const popup = new mapboxgl.Popup({
-              offset: [0, -15]
-            })
-            .setLngLat(e.lngLat)
-            .setHTML(popupContent)
-            .addTo(map);
-        });
-
-      // https://www.mapbox.com/mapbox-gl-js/example/filter-features-within-map-view/
-      map.on('moveend', () => {
-        const features = map.queryRenderedFeatures({
-          layers: ['richmondpointsall']
-        })
-        //console.log(features)
-
-      });
-
-      map.on('zoom', () => {
-        // console.log("mapZoom:", map.getZoom())
-      });
+  map.on('zoom', () => {
+    // console.log("mapZoom:", map.getZoom())
+  });
